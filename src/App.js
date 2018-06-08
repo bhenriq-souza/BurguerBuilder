2d677939d8d5a1488970ad14fe8e06f2b03202be
bhenriq-souza <bhenriq.souza@gmail.com>
2018-06-08 17:29:41 -0300

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import Layout from './hoc/Layout/Layout';
+import { Route, Switch } from 'react-router-dom';
+
 import BurguerBuilder from '../src/containers/BurguerBulder/BurguerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Layout from './hoc/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurguerBuilder />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurguerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Burguer from '../../Burguer/Burguer';
+import Button from '../../Comum/Button/Button';
+import style from './CheckoutSummary.css';
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={style.CheckoutSummary} >
+            <h1>We hope it taste well!</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Burguer ingredients={props.ingredients} />
+            </div>
+            <div>
+                <Button btnType="Danger"
+                        clicked=""
+                >
+                        CANCEL
+                </Button>
+                <Button btnType="Success"
+                        clicked=""
+                >
+                        CONTINUE
+                </Button>
+            </div>
+        </div>
+    );
+
+}
+
+export default checkoutSummary;
